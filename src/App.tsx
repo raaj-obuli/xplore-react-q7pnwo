5fa4d07433a92dc8f940e6b4c847bb4367b5583d
raaj-obuli <raaj.obuli@gmail.com>
2024-04-20 15:46:55 +0530

    usecallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import './App.css';
 
 const useCb = (cb: any, dep: any[]) => {
   const memCb = useCallback(cb, dep);
+
   console.log('=== is same cb', cb === memCb);
+
   return memCb;
 };
 
 function App() {
-  useCb(() => 1, []);
-  return <>Hello</>;
+  const [count, setCount] = useState(0);
+  useCb(() => 1, [count]);
+  const onClick = useCallback(() => setCount(count + 1), [count]);
+
+  return <button onClick={onClick}>Count</button>;
 }
 
 export default App;
